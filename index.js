77503d48309cfe9e1c6cf832e1d9d1c9bef8988c
ooluchi <ooluchiteeto@gmail.com>
2023-02-13 23:21:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('HelloWorld');
+
+
+function multiply ( x, y) {
+    return x * y;
+}
+
+console.log(multiply(30, 5))
+
+let firstName = 'Oluchi Okoro';
+let lastName = 'Okoro';
+let stuterntrack = 'Frontend';
+let skills = 'coding';
+let favoritecolor = 'black';
+let independence = '1960';
+
+console.log(myName);
+console.log(stuterntrack);
+console.log(skills);
+console.log(favoritecolor);
+console.log(independence);
+
+console.log("My name is " + myName + ". I am learning " + stuterntrack ". I love  + skills ". My favorite color is" + favoritecolor + "My country gained independence in" + independence);
---
+++ readme.md
@@ -0,0 +1,7 @@
+Node.js
+JavaScript can be used to build mobile apps, websites, games, server applications and web servers.
+ECMAScript was first released in June, 1997.
+console.log('')
+Backend, Mobile development, DevOps, Full Stack Development, Security Engineer
+If a variable's type can be determined at compile time, a programming language is said to be statically typed. If a variable's type is verified while the program is running, the language is dynamically typed.
+Undefined is a type, whereas null an object.
